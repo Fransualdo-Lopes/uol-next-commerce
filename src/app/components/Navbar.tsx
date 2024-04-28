c74ac72cd8f2d962c75954a7dc789d8b2f2cc55b
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-04-28 01:13:04 -0300

    adicionando componenetes de produtos

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,29 @@
 import Link from "next/link";
 
-function Navabar(){
-    return (
-    <nav className='fixed top-0 w-full flex items-center py-2 px-8 justfy-between z-50 bg-slate-800 text-gray-300'>
-    <Link href="/" className='uppercase font-bold text-md h-12 flex items-center'>
-      Next Store
-    </Link>
-  </nav>
-  ) 
+function Navabar() {
+  return (
+    <nav className='fixed top-0 w-full flex items-center py-3 px-8 justfy-between z-50 bg-white text-black'>
+      <Link href="/" className='font-bold text-md h-12 flex items-center'>
+        Furniro
+      </Link>
+
+      {/* Menu de navegação centralizado */}
+      <ul className="flex items-center justify-center space-x-8 flex-grow">
+        <li>
+          <Link href="/" className="hover:text-gray-800">Home</Link>
+        </li>
+        <li>
+          <Link href="/shop" className="hover:text-gray-800">Shop</Link>
+        </li>
+        <li>
+          <Link href="/about" className="hover:text-gray-800">About</Link>
+        </li>
+        <li>
+          <Link href="/contact" className="hover:text-gray-800">Contact</Link>
+        </li>
+      </ul>
+
+    </nav>
+  )
 }
 export default Navabar;
---
+++ src/types/ProductType.ts
@@ -0,0 +1,9 @@
+export Type ProductType = {
+    id: string;
+    title: string;
+    price: number | null;
+    description: string | null; 
+    image: string;
+    category: string;
+
+}
