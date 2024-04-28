ae40b0224a87525bb39a56737044f7a996d46c81
Fransualdo Lopes <fransualdo.14@gmail.com>
2024-04-27 22:56:34 -0300

    listagem de produtos primeira parte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className='bg-slate-200 h-screen p-16'>
+        <main className='bg-slate-600 h-screen p-16'>
           {children}
         </main>
       </body>
